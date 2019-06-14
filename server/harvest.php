b2e67d4a3aad63c5c82c8716416bab96b979e924
djagya <danil.kabluk@gmail.com>
2019-06-14 09:48:34 +0200

    fix

--- server/harvest.php
+++ server/harvest.php
@@ -7,6 +7,7 @@ use Search\Indexes;
 require 'vendor/autoload.php';
 
 $index = $argv[1] ?? null;
+$count = $argv[2] ?? 1;
 if (!$index) {
     echo "Usage: docker-compose exec app php server/harvest.php {epf|spins}\n";
 
@@ -18,10 +19,10 @@ echo sprintf("Running in %s mode\n", getenv('ENV') === 'production' ? 'productio
 Indexes::init($index);
 
 if ($index === Indexes::SPINS_IDX) {
-    SpinsHarvester::run(5);
+    SpinsHarvester::run($count);
 } elseif ($index === Indexes::EPF_IDX) {
     // EPF data are expensive to load and won't gain from multiple running harvesters.
-    EpfHarvester::run(1);
+    EpfHarvester::run($count);
 } else {
     echo "Invalid index\n";
 }
